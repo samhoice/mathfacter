8fd2f6e938b2770c76fae5007e61579c95964efd
Jonathan Adelson <jon@adelson.io>
2020-05-24 10:09:16 -0400

    Model updates, bug fixes, divide by zero protection

--- problems/models.py
+++ problems/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import random
 
 
@@ -8,16 +9,21 @@ class Rule(models.Model):
     left = models.IntegerField()
     right = models.IntegerField()
 
+    def __str__(self):
+        return "{} {} {}".format(self.left, self.allowed_ops, self.right)
+
     def question_from_rule(self):
         left = random.randint(0, self.left)
         right = random.randint(0, self.right)
+        op = random.choice(self.allowed_ops)
 
-        q = Question.object.get_or_create(
-            rule=self, operation=self.op, left_hand=left, right_hand=right
+        q = Question.objects.get_or_create(
+            rule=self, operation=op, left_hand=left, right_hand=right
         )
-        q.calculate()
 
-        return q
+        q[0].calculate()
+
+        return q[0]
 
 
 class Question(models.Model):
@@ -30,6 +36,9 @@ class Question(models.Model):
     right_hand = models.IntegerField()
     result = models.IntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return "{} {} {}".format(self.left_hand, self.operation, self.right_hand)
+
     def calculate(self):
         if self.operation == "+":
             self.result = self.left_hand + self.right_hand
@@ -38,10 +47,17 @@ class Question(models.Model):
         elif self.operation == "*":
             self.result = self.left_hand * self.right_hand
         elif self.operation == "/":
-            self.result = self.left_hand / self.right_hand
+            if self.right_hand == 0:
+                self.result = None
+            else:
+                self.result = self.left_hand / self.right_hand
 
 
 class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     value = models.IntegerField()
     correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{}".format(self.value)
