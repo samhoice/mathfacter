b672ee70ec0c8f0e701423a84f8ad7b10fc4f45a
Jonathan Adelson <jon@adelson.io>
2020-06-16 17:07:00 -0400

    Adds a simple question generator

--- problems/views.py
+++ problems/views.py
@@ -3,6 +3,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Rule, Question
 from .serializers import UserSerializer, QuestionSerializer, RuleSerializer
 
@@ -27,3 +29,15 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False, methods=["get"])
+    def question(self, request, *args, **kwargs):
+        """ endpoint for getting a question
+
+        detail is false becaues you can't ask for a question by pk
+        """
+        rule = Rule.objects.latest("pk")
+        question = rule.question_from_rule()
+
+        serializer = self.get_serializer(question)
+        return Response(serializer.data)
