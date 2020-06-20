353eee74566320b0184dd06a723db382d1e819cf
Jonathan Adelson <jon@adelson.io>
2020-06-20 12:02:02 -0400

    Adds auth url for drf

--- mathfacter/urls.py
+++ mathfacter/urls.py
@@ -27,4 +27,5 @@ router.register(r"problems", views.ProblemViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
